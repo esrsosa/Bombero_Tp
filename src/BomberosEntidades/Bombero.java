f882e246c618732b85517f3d2cd7e4564106e088
esrsosa <131368173+esrsosa@users.noreply.github.com>
2023-10-25 15:27:44 -0300

    principal

--- src/BomberosEntidades/Bombero.java
+++ src/BomberosEntidades/Bombero.java
@@ -4,13 +4,9 @@
  * and open the template in the editor.
  */
 package BomberosEntidades;
-
-<<<<<<< HEAD
 import java.util.Date;
-=======
-import java.sql.Date;
 import java.time.LocalDate;
->>>>>>> 954e5de9f2e945beccd0f25c695e78216336df88
+
 
 /**
  *
@@ -101,92 +97,4 @@ public class Bombero {
         this.estado = estado;
     }
     
-<<<<<<< HEAD
-    private int codBombero ;
-    private int dni;
-    private String nombre;
-    private String apellido;
-    private String grupoSanguineo;
-    private Date fechaNacimiento;
-    private int celular;
-    private boolean activo;
-
-    public Bombero(int codBombero, int dni, String nombre, String apellido, String grupoSanguineo, Date fechaNacimiento, int celular, boolean activo) {
-        this.codBombero = codBombero;
-        this.dni = dni;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.grupoSanguineo = grupoSanguineo;
-        this.fechaNacimiento = fechaNacimiento;
-        this.celular = celular;
-        this.activo = activo;
-    }
-
-    public int getCodBombero() {
-        return codBombero;
-    }
-
-    public void setCodigoBombero(int codBombero) {
-        this.codBombero = codBombero;
-    }
-
-    public int getDni() {
-        return dni;
-    }
-
-    public void setDni(int dni) {
-        this.dni = dni;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getGrupoSanguineo() {
-        return grupoSanguineo;
-    }
-
-    public void setGrupoSanguineo(String grupoSanguineo) {
-        this.grupoSanguineo = grupoSanguineo;
-    }
-
-    public Date getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    public void setFechaNacimiento(Date fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
-
-    public int getCelular() {
-        return celular;
-    }
-
-    public void setCelular(int celular) {
-        this.celular = celular;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
-    
-=======
-
->>>>>>> 954e5de9f2e945beccd0f25c695e78216336df88
 }
