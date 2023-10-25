954e5de9f2e945beccd0f25c695e78216336df88
esrsosa <131368173+esrsosa@users.noreply.github.com>
2023-10-24 23:20:52 -0300

    zzz

---
+++ bomberos.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-10-2023 a las 03:11:55
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bomberos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bombero`
+--
+
+CREATE TABLE `bombero` (
+  `id_bombero` int(11) NOT NULL,
+  `dni` varchar(8) NOT NULL,
+  `nombre_ape` varchar(50) NOT NULL,
+  `fecha_nac` date NOT NULL,
+  `celular` varchar(15) NOT NULL,
+  `codBrigada` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `brigada`
+--
+
+CREATE TABLE `brigada` (
+  `codBrigada` int(11) NOT NULL,
+  `nombre_br` varchar(20) NOT NULL,
+  `especialidad` varchar(30) NOT NULL,
+  `libre` tinyint(1) NOT NULL,
+  `nro_cuartel` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuartel`
+--
+
+CREATE TABLE `cuartel` (
+  `codCuartel` int(11) NOT NULL,
+  `nombre_cuartel` varchar(20) NOT NULL,
+  `direccion` varchar(30) NOT NULL,
+  `coord_x` int(11) NOT NULL,
+  `coord_y` int(11) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `correo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `siniestro`
+--
+
+CREATE TABLE `siniestro` (
+  `codigo` int(11) NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  `fecha_siniestro` date NOT NULL,
+  `coord_x` int(11) NOT NULL,
+  `coord_y` int(11) NOT NULL,
+  `detalles` text NOT NULL,
+  `fecha_resol` date NOT NULL,
+  `puntuacion` int(11) NOT NULL,
+  `codBrigada` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `bombero`
+--
+ALTER TABLE `bombero`
+  ADD PRIMARY KEY (`id_bombero`),
+  ADD KEY `codBrigada` (`codBrigada`);
+
+--
+-- Indices de la tabla `brigada`
+--
+ALTER TABLE `brigada`
+  ADD PRIMARY KEY (`codBrigada`),
+  ADD KEY `nro_cuartel` (`nro_cuartel`);
+
+--
+-- Indices de la tabla `cuartel`
+--
+ALTER TABLE `cuartel`
+  ADD PRIMARY KEY (`codCuartel`);
+
+--
+-- Indices de la tabla `siniestro`
+--
+ALTER TABLE `siniestro`
+  ADD PRIMARY KEY (`codigo`),
+  ADD KEY `codBrigada` (`codBrigada`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `bombero`
+--
+ALTER TABLE `bombero`
+  ADD CONSTRAINT `bombero_ibfk_1` FOREIGN KEY (`codBrigada`) REFERENCES `brigada` (`codBrigada`);
+
+--
+-- Filtros para la tabla `cuartel`
+--
+ALTER TABLE `cuartel`
+  ADD CONSTRAINT `cuartel_ibfk_1` FOREIGN KEY (`codCuartel`) REFERENCES `brigada` (`nro_cuartel`);
+
+--
+-- Filtros para la tabla `siniestro`
+--
+ALTER TABLE `siniestro`
+  ADD CONSTRAINT `siniestro_ibfk_1` FOREIGN KEY (`codBrigada`) REFERENCES `brigada` (`codBrigada`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/BomberosEntidades/Bombero.java
+++ src/BomberosEntidades/Bombero.java
@@ -5,10 +5,97 @@
  */
 package BomberosEntidades;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 /**
  *
  * @author Emanuel Sosa
  */
 public class Bombero {
+    private int id_bombero;
+    private String dni;
+    private String nombre_ape;
+    private String grupSanguineo;
+    private LocalDate fecha_nac;
+    private String celular;
+    private int codBrigada;
+    private boolean estado;
+
+    public Bombero(int id_bombero, String dni, String nombre_ape, String grupSanguineo, LocalDate fecha_nac, String celular, int codBrigada, boolean estado) {
+        this.id_bombero = id_bombero;
+        this.dni = dni;
+        this.nombre_ape = nombre_ape;
+        this.grupSanguineo = grupSanguineo;
+        this.fecha_nac = fecha_nac;
+        this.celular = celular;
+        this.codBrigada = codBrigada;
+        this.estado = estado;
+    }
+
+    public int getId_bombero() {
+        return id_bombero;
+    }
+
+    public void setId_bombero(int id_bombero) {
+        this.id_bombero = id_bombero;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre_ape() {
+        return nombre_ape;
+    }
+
+    public void setNombre_ape(String nombre_ape) {
+        this.nombre_ape = nombre_ape;
+    }
+
+    public String getGrupSanguineo() {
+        return grupSanguineo;
+    }
+
+    public void setGrupSanguineo(String grupSanguineo) {
+        this.grupSanguineo = grupSanguineo;
+    }
+
+    public LocalDate getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nac(LocalDate fecha_nac) {
+        this.fecha_nac = fecha_nac;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public int getCodBrigada() {
+        return codBrigada;
+    }
+
+    public void setCodBrigada(int codBrigada) {
+        this.codBrigada = codBrigada;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
     
+
 }
