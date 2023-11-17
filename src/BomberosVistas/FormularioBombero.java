4439290fcf323c582c61366ed1523d5beb72bd22
esrsosa <131368173+esrsosa@users.noreply.github.com>
2023-11-17 13:38:12 -0300

    z

--- src/BomberosVistas/FormularioBombero.java
+++ src/BomberosVistas/FormularioBombero.java
@@ -43,7 +43,6 @@ public class FormularioBombero extends javax.swing.JInternalFrame {
         ArmarTabla();
         armarSangre();
         llenarTabla();
-//        llenarTipo();
         llenarComboBox();
         brigadas();
     }
