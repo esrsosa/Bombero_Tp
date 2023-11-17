842002571603bf104cfbb7d1852e1f59c59aeb95
ariansara <102766969+ariansara@users.noreply.github.com>
2023-11-17 11:27:22 -0300

    cuartel data

--- src/BomberoAcceesoDatos/CuartelData.java
+++ src/BomberoAcceesoDatos/CuartelData.java
@@ -89,7 +89,7 @@ public class CuartelData {
     }
 
     public List<Cuartel> listaCuarteles (){
-        ArrayList<Cuartel> cuarteles = new ArrayList<>();
+        List<Cuartel> cuarteles = new ArrayList<>();
         String sql = "SELECT  codCuartel, nombre_cuartel, direccion, telefono, correo, activo FROM cuartel WHERE 1";
         try{
             PreparedStatement ps = con.prepareStatement(sql);
