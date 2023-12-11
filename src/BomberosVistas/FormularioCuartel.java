855682f9a1cd5674231687cb7a48b59ab99297ac
ariansara <102766969+ariansara@users.noreply.github.com>
2023-12-11 17:59:19 -0300

    cambio

--- src/BomberosVistas/FormularioCuartel.java
+++ src/BomberosVistas/FormularioCuartel.java
@@ -120,27 +120,27 @@ public class FormularioCuartel extends javax.swing.JInternalFrame {
         jLabel1.setBackground(new java.awt.Color(0, 0, 0));
         jLabel1.setFont(new java.awt.Font("Yu Gothic Light", 0, 14)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("Nombre del Cuartel");
+        jLabel1.setText("Nombre del Cuartel:");
 
         jLabel2.setBackground(new java.awt.Color(0, 0, 0));
         jLabel2.setFont(new java.awt.Font("Yu Gothic Light", 0, 14)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("Direccion");
+        jLabel2.setText("Direccion:");
 
         jLabel3.setBackground(new java.awt.Color(0, 0, 0));
         jLabel3.setFont(new java.awt.Font("Yu Gothic Light", 0, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setText("Coordenadas X");
+        jLabel3.setText("Coordenadas X:");
 
         jLabel4.setBackground(new java.awt.Color(0, 0, 0));
         jLabel4.setFont(new java.awt.Font("Yu Gothic Light", 0, 14)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel4.setText("Coordenadas Y");
+        jLabel4.setText("Coordenadas Y:");
 
         jLabel5.setBackground(new java.awt.Color(0, 0, 0));
         jLabel5.setFont(new java.awt.Font("Yu Gothic Light", 0, 14)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel5.setText("Teléfono");
+        jLabel5.setText("Teléfono:");
 
         jLabel6.setBackground(new java.awt.Color(0, 0, 0));
         jLabel6.setFont(new java.awt.Font("Yu Gothic Light", 0, 14)); // NOI18N
